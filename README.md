9b12be6d3fa180ab2d8f07cfbc2f122cba9745b2
Jaemilton <jaemiltonro@yahoo.com.br>
2023-02-15 01:13:22 -0300

    ajustes

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ python internet_speed_test.py --csv --output-file my_speed_test_results.csv
 python internet_speed_test.py --database "--host", "mysql_hostname", "--user", "username", "--password", "your_password", "--database_name", "internet_speed_test"
 
 #to schedule executions using cron add the following line do crontab -e
-0 5 * * 1 tar -zcf /var/backups/home.tgz /home/
+*/20 * * * * /root/git/internet_speed_test/internet_speed_test.py --database --host mysql.rede.jai --user internet_monitor --password EoiV@BvA9u3Nrc --database_name internet_monitor 
 
 
---
+++ runner.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+SECONDS=0
+MYSQLHOST=$1
+DATABASE=$2
+
+full_path=$(realpath $0)
+dir_path=$(dirname $full_path)
+echo "dir_path=$dir_path"
+
+if [ -f  $dir_path/.env ]
+then
+  #echo ">>>>>> reading variables from $dir_path/.env"
+  export $(cat $dir_path/.env | sed 's/#.*//g' | xargs)
+else
+  echo ">>>>>> variables file $dir_path/.env not found"
+fi
+
+USR_VAR=USR_${DATABASE^^}
+USERNAME=${!USR_VAR}
+
+PWD_VAR=PWD_${DATABASE^^}
+PASSWORD=${!PWD_VAR}
+
+/bin/python3 /root/git/internet_speed_test/internet_speed_test.py --database --host $MYSQLHOST --user $USERNAME --password $PASSWORD --database_name $DATABASE
+
+eval "echo Elapsed time: $(date -ud "@$SECONDS" +'$((%s/3600/24)) days %H hr %M min %S sec')"
